98f43602c5eecc34f580edecc8e111b0ae4a3deb
atranberg <101251138+atranberg@users.noreply.github.com>
2022-06-18 14:29:25 +0200

    Created the react app and the basic index page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import Search from "./Search.js";
+import CurrentWeather from "./CurrentWeather.js";
+import SunWind from "./SunWind.js";
+import Footer from "./Footer.js";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <Search />
+      <CurrentWeather />
+      <SunWind />
+      <Footer />
+    </div>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
