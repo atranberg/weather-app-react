e92a20668949cf261e7d9c0058b28a646da719ea
atranberg <101251138+atranberg@users.noreply.github.com>
2022-06-18 17:58:15 +0200

    Updated link to GitHub repository

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,10 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/atranberg/weather-app">Open source code</a> by
-      Anna Tranberg
+      <a href="https://github.com/atranberg/weather-app-react">
+        Open source code
+      </a>{" "}
+      by Anna Tranberg
     </div>
   );
 }
