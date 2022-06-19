796729964035d76a74a99c7b58fffec7a8778a5e
atranberg <101251138+atranberg@users.noreply.github.com>
2022-06-19 14:30:48 +0200

    Updated Index page to remove SunWind and added default city to CurrentWeather

--- src/index.js
+++ src/index.js
@@ -2,22 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Search from "./Search";
 import CurrentWeather from "./CurrentWeather";
-import SunWind from "./SunWind";
 import Footer from "./Footer";
-import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Search />
-    <CurrentWeather />
-    <SunWind />
+    <CurrentWeather defaultCity="Borås" />
     <Footer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
