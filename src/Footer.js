c613418bdc52a244f75de5b76c1d333bf5df67ea
atranberg <101251138+atranberg@users.noreply.github.com>
2022-07-03 10:57:27 +0200

    Added target and rel to Footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/atranberg/weather-app-react">
+      <a
+        href="https://github.com/atranberg/weather-app-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Open source code
       </a>{" "}
       by Anna Tranberg
